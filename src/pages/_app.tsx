3155c25ac5634f55d19a5bf980669e5f8cadaf43
Ahmed Tazi <xdieselx09@gmail.com>
2023-04-21 15:56:00 +0400

    Addition of Toaster Wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,24 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-import { api } from "../utils/api";
+import { api } from "../utils";
 
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-      <Analytics />
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+        <Analytics />
+      </SessionProvider>
+      <Toaster />
+    </>
   );
 };
 
